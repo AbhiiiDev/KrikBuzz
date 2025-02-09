f1ed0f3ea14fda4552e0dea43258028f00d5f934
AbhiiiDev <http://localhost:9000>
2025-02-10 00:15:06 +0530

    drawer component color changed and fixed

--- KrikBuzz/src/components/DrawerContent.jsx
+++ KrikBuzz/src/components/DrawerContent.jsx
@@ -1,67 +1,25 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 
-const DrawerContent = ({dropOptions,teamList}) => {
-  const [teamHover, setTeamHover] = useState(false);
-  const [hover, setHover] = useState(false);
-  const handleMouseEnter = () => {
-    setHover(true);
-  };
-
-  const handleMouseLeave = () => {
-    setHover(false);
-  };
+const DrawerContent = () => {
   return (
-    <div>
-      <ul className="  flex flex-col gap-8">
+    <div className='bg-primary'>
+      <ul className="flex flex-col gap-8 font-semibold ">
                 <Link to='/livescores'>
                 <li className=" cursor-pointer">Live Scores</li>
                 </Link>
                 <li className="cursor-pointer">Schedules</li>
                 <li
                   className="cursor-pointer relative"
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
                 >
                   News
-                  {hover && (
-                    <ul className="absolute top-full -right-10 text-black bg-white shadow-md w-36 ">
-                      {dropOptions.slice(0, 8).map((obj) => {
-                        return (
-                          <li key={obj.id} className="py-2 px-4 hover:bg-gray-200">
-                            {obj.name}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  )}
                 </li>
-      
                 <li className=" cursor-pointer">Series</li>
                 <li
                   className=" cursor-pointer relative"
-                  onMouseEnter={() => setTeamHover(true)}
-                  onMouseLeave={() => setTeamHover(false)}
                 >
                   Teams
-                  {teamHover && (
-                    <ul className="absolute top-full -right-10 text-black bg-white shadow-md w-36 ">
-                      <div>
-                        {teamList.slice(1, 13).map((obj) => {
-                          return (
-                            <li
-                              key={obj.TeamId}
-                              className="py-2 px-4 hover:bg-gray-200"
-                            >
-                              {obj.teamName}
-                            </li>
-                          );
-                        })}
-                      </div>
-                    </ul>
-                  )}
                 </li>
                 <li className="cursor-pointer">Rankings</li>
               </ul>
--- KrikBuzz/src/components/Header.jsx
+++ KrikBuzz/src/components/Header.jsx
@@ -7,26 +7,23 @@ import { useQuery } from "@tanstack/react-query";
 import { getNewsCategory, getTeamList } from "../api/categories";
 import DrawerContent from "./DrawerContent";
 
-
 const Header = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [teamHover, setTeamHover] = useState(false);
   const [hover, setHover] = useState(false);
   // const [dropOptions, setDropOptions] = useState([]);
-  const {data:dropOptions} =useQuery({
-    queryKey:["newsCategory"],
-    queryFn:getNewsCategory,
+  const { data: dropOptions } = useQuery({
+    queryKey: ["newsCategory"],
+    queryFn: getNewsCategory,
     staleTime: 5 * 60 * 1000, // Cache for 5 mins
     cacheTime: 10 * 60 * 1000, // Keep data for 10 mins
-  })
-  const {data:teamList} =useQuery({
-    queryKey:["teamList"],
-    queryFn:getTeamList,
+  });
+  const { data: teamList } = useQuery({
+    queryKey: ["teamList"],
+    queryFn: getTeamList,
     staleTime: 5 * 60 * 1000, // Cache for 5 mins
     cacheTime: 10 * 60 * 1000, // Keep data for 10 mins
-  })
-
-
+  });
 
   const handleMouseEnter = () => {
     setHover(true);
@@ -38,18 +35,18 @@ const Header = () => {
 
   return (
     <div className="flex justify-between items-center bg-[#09ae84]  px-4">
-        <Link to='/'>
-      <img
-        className="h-20 cursor-pointer"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6fe_4zNybRyHSfe3rVymQ2E3jD2-d-AfSJv583qglLFKiWIXKV9BHgkABF7dfaRk6M&usqp=CAU"
-        alt="logo"
-      />
-       </Link>
+      <Link to="/">
+        <img
+          className="h-20 cursor-pointer"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6fe_4zNybRyHSfe3rVymQ2E3jD2-d-AfSJv583qglLFKiWIXKV9BHgkABF7dfaRk6M&usqp=CAU"
+          alt="logo"
+        />
+      </Link>
 
       <div className="hidden md:block">
         <ul className="text-white text-center flex gap-8 ">
-          <Link to='/livescores'>
-          <li className=" cursor-pointer">Live Scores</li>
+          <Link to="/livescores">
+            <li className=" cursor-pointer">Live Scores</li>
           </Link>
           <li className="cursor-pointer">Schedules</li>
           <li
@@ -105,14 +102,30 @@ const Header = () => {
           </Button>
       </div> */}
       <div className="md:hidden">
-      <Drawer opened={opened} onClose={close} title="KrikBuzz">
-        {/* Drawer content */}
-        <DrawerContent dropOptions={dropOptions} teamList={teamList}/>
-      </Drawer>
+        <Drawer
+          styles={{
+            body: { backgroundColor: "#09ae84" },
+            header: { backgroundColor: "#09ae84" },
+          }}
+          opened={opened}
+          onClose={close}
+          title={
+            <Link to="/">
+              <img
+                className="h-20 cursor-pointer"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6fe_4zNybRyHSfe3rVymQ2E3jD2-d-AfSJv583qglLFKiWIXKV9BHgkABF7dfaRk6M&usqp=CAU"
+                alt="logo"
+              />
+            </Link>
+          }
+        >
+          {/* Drawer content */}
+          <DrawerContent />
+        </Drawer>
 
-  <ActionIcon variant="transparent" color="dark"  onClick={open}>
-      <FaHamburger  size={21}/>
-  </ActionIcon>
+        <ActionIcon variant="transparent" color="dark" onClick={open}>
+          <FaHamburger size={21} />
+        </ActionIcon>
       </div>
     </div>
   );
